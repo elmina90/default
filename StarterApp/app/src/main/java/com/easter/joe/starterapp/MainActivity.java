145357a0ca3d08582f4f52a80db755196ae8793a
Joe <budigunawanjohanes@gmail.com>
2016-01-24 03:32:59 +0700

    rename tab

--- StarterApp/app/src/main/java/com/easter/joe/starterapp/MainActivity.java
+++ StarterApp/app/src/main/java/com/easter/joe/starterapp/MainActivity.java
@@ -124,11 +124,11 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "TAB 1";
+                    return "SECTION 1";
                 case 1:
-                    return "TAB 2";
+                    return "SECTION 2";
                 case 2:
-                    return "TAB 3";
+                    return "SECTION 3";
             }
             return null;
         }
